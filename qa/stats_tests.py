648277d8e016f31002a6fb72fda0d5f9a2feff40
Anton TonyL Lysakov <tlysakov@gmail.com>
2021-02-08 18:52:43 +0700

    added fixture for orderbook stats tests

--- qa/stats_tests.py
+++ qa/stats_tests.py
@@ -2,22 +2,25 @@ import unittest
 import sys
 sys.path.append("..")
 import stats_utils
+import json
 
 
 class FindLowestAskTest(unittest.TestCase):
 
     def test(self):
-        orderbook = {}
-        lowest_ask = stats_utils.find_lowest_ask(orderbook)
-        self.assertEqual(lowest_ask, 0)
+        with open("fixtures/orderbook_btc_kmd.json") as orderbook_json:
+            orderbook_fixture = json.load(orderbook_json)
+        lowest_ask = stats_utils.find_lowest_ask(orderbook_fixture)
+        self.assertEqual(lowest_ask, "48626.15089487")
 
 
 class FindHighestBidTest(unittest.TestCase):
 
     def test(self):
-        orderbook = {}
-        highest_bid = stats_utils.find_highest_bid(orderbook)
-        self.assertEqual(highest_bid, 0)
+        with open("fixtures/orderbook_btc_kmd.json") as orderbook_json:
+            orderbook_fixture = json.load(orderbook_json)
+        highest_bid = stats_utils.find_highest_bid(orderbook_fixture)
+        self.assertEqual(highest_bid, "44984.25551057130004498425551057130004498425551057130004498425551057130004498425551057130004498425551")
 
 
 if __name__ == '__main__':
