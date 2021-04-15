91f2079d15a6e793f4e0c0bce947cb00f57ddef7
Anton TonyL Lysakov <tlysakov@gmail.com>
2021-04-15 14:42:39 +0700

    fix bug

--- stats_utils.py
+++ stats_utils.py
@@ -370,7 +370,7 @@ def volume_for_ticker(ticker, path_to_db, days_in_past):
     previous_volume = 0
     for i in range(0, days_in_past):
         overall_volume = 0
-        ticker_data = ticker_for_ticker(ticker, path_to_db, days_in_past)
+        ticker_data = ticker_for_ticker(ticker, path_to_db, i+1)
         d = (datetime.today() - timedelta(days=i)).strftime('%Y-%m-%d')
         volumes_dict[d] = 0
         for pair in ticker_data:
