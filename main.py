94bfbd5ed66165312434a0b6210b4e46c70075d3
Anton 'TonyL' Lysakov <tlysakov@gmail.com>
2021-05-03 09:09:49 +0200

    ssl

--- main.py
+++ main.py
@@ -91,4 +91,4 @@ def volumes_history_ticker(ticker_vol="KMD", days_in_past=1):
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, ssl_keyfile="/etc/letsencrypt/live/stats.testchain.xyz/privkey.pem", ssl_certfile="/etc/letsencrypt/live/stats.testchain.xyz/fullchain.pem")
