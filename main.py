43b50fca778a5ee16188f77a20aadef4b61ae694
Anton 'TonyL' Lysakov <tlysakov@gmail.com>
2021-08-04 08:23:44 +0200

    Revert "count usd volume"
    This reverts commit 88620bebc2dc769e8caa99e80937572f0f199065.

--- main.py
+++ main.py
@@ -37,34 +37,11 @@ def cache_gecko_data():
 def cache_summary_data():
     available_pairs_summary = get_availiable_pairs(path_to_db)
     summary_data = []
-    with open('gecko_cache.json', 'r') as json_file:
-        gecko_cached_data = json.load(json_file)
-    total_usd_volume = 0
     for pair in available_pairs_summary:
         pair_summary = summary_for_pair(pair, path_to_db)
         summary_data.append(pair_summary)
-        try:
-            base_currency_usd_vol = gecko_cached_data[pair_summary["base_currency"]]["usd_price"]\
-                                    * pair_summary["base_volume"]
-        except KeyError:
-            base_currency_usd_vol = 0
-        try:
-            quote_currency_usd_vol = gecko_cached_data[pair_summary["quote_currency"]]["usd_price"]\
-                                     * pair_summary["quote_currency"]
-        except KeyError:
-            quote_currency_usd_vol = 0
-        # a bit hacky way but its semantically not correct to add both, so we adding most valuable one volume
-        if base_currency_usd_vol > quote_currency_usd_vol:
-            total_usd_volume += base_currency_usd_vol
-        else:
-            total_usd_volume += quote_currency_usd_vol
-    # cache for summary api call
     with open('summary_cache.json', 'w+') as json_file:
         json.dump(summary_data, json_file)
-    # cache for usd_volume
-    usd_volume = {"usd_volume_24h": total_usd_volume}
-    with open('usd_volume_cache.json', 'w+') as json_file:
-        json.dump(usd_volume, json_file)
 
 
 @app.get('/api/v1/summary')
@@ -72,13 +49,6 @@ def summary():
     with open('summary_cache.json', 'r') as json_file:
         summary_cache_data = json.load(json_file)
         return summary_cache_data
-
-
-@app.get('/api/v1/usd_volume')
-def summary():
-    with open('usd_volume_cache.json', 'r') as json_file:
-        usd_volume_cache_data = json.load(json_file)
-        return usd_volume_cache_data
 
 
 @app.get('/api/v1/summary_for_ticker/{ticker_summary}')
