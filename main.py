be2d443772882502cf69ff24146237f8b4497ad4
Anton TonyL Lysakov <tlysakov@gmail.com>
2022-05-31 11:54:41 +0200

    use static data for website call

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
+import json
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_utils.tasks import repeat_every
 from stats_utils import get_availiable_pairs, summary_for_pair, ticker_for_pair, orderbook_for_pair, trades_for_pair, atomicdex_info
 
 path_to_db = '/DB/43ec929fe30ee72be42c9162c56dde910a05e50d/MM2.db'
@@ -46,10 +48,20 @@ def trades(market_pair="KMD_BTC"):
     return trades_data
 
 
+@app.on_event("startup")
+@repeat_every(seconds=600)  # caching data every 10 minutes
+def cache_atomicdex_io():
+    data = atomicdex_info(path_to_db)
+    with open('adex_cache.json', 'w+') as cache_file:
+        json.dump(data, cache_file)
+
+
 @app.get('/api/v1/atomicdexio')
 def atomicdex_info_api():
-    data = atomicdex_info(path_to_db)
-    return data
+    with open('adex_cache.json', 'r') as json_file:
+        adex_cached_data = json.load(json_file)
+        return adex_cached_data
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8080)
